eca48703d88bdb7a2250e235c26959656ca31a4f
Evan's Macbook <xuevanj@gmail.com>
2025-11-19 12:02:00 -0500

    feat: baseline platform class

---
+++ views/platform.py
@@ -0,0 +1,51 @@
+"""Platform Class"""
+import pygame
+import random
+
+class Platform:
+    def __init__(self, screen: pygame.Surface, image: pygame.Surface, word: str, existing: list = []):
+        """Initialize Platform"""
+        self.width, self.height = self.get_size(screen)
+        self.x, self.y = self.get_random_coords(screen, existing)
+        self.word = word
+        self.image = pygame.transform.scale(image, (self.width, self.height))
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+
+    def draw(self, screen: pygame.Surface):
+        """Draw the Platform"""
+        screen.blit(self.image, self.rect)
+        self.render_word(screen)
+
+
+    def render_word(self, screen: pygame.Surface):
+        """Render the word on the platform"""
+        self.fit_font()
+        word_surf = self.font.render(self.word, True, (0, 0, 0))
+        word_rect = word_surf.get_rect(center = self.rect.center)
+        screen.blit(word_surf, word_rect)
+
+    def fit_font(self):
+        """Fit Font Size to Platform"""
+        font_size = self.height // 3
+        self.font = pygame.font.Font("assets/Kenney Future Narrow.ttf", font_size)
+        
+
+    def get_size(self, screen: pygame.Surface):
+        """Get Size of Platform"""
+        w = screen.get_size()[0]
+        return w // 4, w // 12
+    
+
+    def get_random_coords(self, screen: pygame.Surface, existing: list) -> tuple[int, int]:
+        """Get Random Coordinates for Platform"""
+        W, H = screen.get_size()
+
+        while True: 
+            x = random.randint(0, W - self.width)
+            y = random.randint(0, H // 2 - self.height)
+
+            new_rect = pygame.Rect(x, y, self.width, self.height)
+
+            if not any(new_rect.colliderect(platform.rect) for platform in existing):
+                return x, y
